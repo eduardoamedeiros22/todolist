2158b7faf78058f4956f20a765ce966960e07707
Eduardo Medeiros <eduardoamedeiros22@gmail.com>
2023-10-17 00:09:20 -0300

    Adição das tarefas

---
+++ src/main/java/br/com/javarocketseat/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,57 @@
+package br.com.javarocketseat.todolist.filter;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.javarocketseat.todolist.user.IUserRepository;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Autowired
+    IUserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        var servletPath = request.getServletPath();
+
+        if (servletPath.startsWith("/tasks")) {
+
+            var authorization = request.getHeader("Authorization");
+
+            var authEncoded = authorization.substring("Basic".length()).trim();
+            byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
+            var authString = new String(authDecoded);
+
+            String[] credentials = authString.split(":");
+            String username = credentials[0];
+            String password = credentials[1];
+
+            var user = this.userRepository.findByUsername(username);
+            if (user == null) {
+                response.sendError(401);
+            } else {
+                var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                if (passwordVerify.verified) {
+                    request.setAttribute("idUser", user.getId());
+                    filterChain.doFilter(request, response);
+                    return;
+                } else {
+                    response.sendError(401);
+                }
+            }
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
+}
--- src/main/java/br/com/javarocketseat/todolist/user/UserController.java
+++ src/main/java/br/com/javarocketseat/todolist/user/UserController.java
@@ -1,6 +1,9 @@
 package br.com.javarocketseat.todolist.user;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,13 +18,20 @@ public class UserController {
     private IUserRepository userRepository;
 
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel){
-        UserModel userverify = userRepository.findByUsername(userModel.getUsername());
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        UserModel verifiedUser = userRepository.findByUsername(userModel.getUsername());
 
-        if(userverify != null){
-            System.out.println("Usuário já existe");
-            return null;
+        if(verifiedUser != null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
-        return this.userRepository.save(userModel);
+
+        var hashredPassword = BCrypt.withDefaults().
+                hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(hashredPassword);
+
+        var userCreated = userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
+
 }
